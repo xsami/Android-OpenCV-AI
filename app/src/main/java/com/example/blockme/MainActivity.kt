47265bc298b646c589de68d494c84f1a06db8740
xsami <samil.vargas@hotmail.com>
2019-06-24 19:13:36 -0400

    add: OpenCVLoader debug initialized

--- app/src/main/java/com/example/blockme/MainActivity.kt
+++ app/src/main/java/com/example/blockme/MainActivity.kt
@@ -2,18 +2,29 @@ package com.example.blockme
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if ( OpenCVLoader.initDebug() ) {
+            System.out.println("Succed")
+            Toast.makeText(applicationContext, "OpenCV loaded successsfully", Toast.LENGTH_SHORT).show()
+        } else {
+            System.out.println("Failed")
+
+            Toast.makeText(applicationContext, "Could not load OpenCV", Toast.LENGTH_SHORT).show()
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
